54c1d3f3acd89f244aded8b7895abf07cfc8c183
kjartanleander <Kjar0321@stud.kea.dk>
2023-11-08 18:53:02 +0100

    made another js file and added the js from html. Made the response we recieve better to read.

---
+++ js/script.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("specForm");
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        // Get form data
+        const primaryUsage = document.getElementById("usage").value;
+        const formFactor = document.getElementById("hardware").value;
+        const budget = parseFloat(document.getElementById("budget").value);
+        const other = document.getElementById("others").value;
+
+        // Prepare data object
+        const data = {
+            primaryUsage: primaryUsage,
+            formFactor: formFactor,
+            budget: budget,
+            other: other
+        };
+
+        // Send data to backend API
+        fetch("http://localhost:8082/chat", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+            .then(response => response.json())
+            .then(apiResponse => {
+                // Handle the response from the backend API
+                console.log(apiResponse);
+
+                // Extract relevant information from the response
+                const content = apiResponse[0].message.content;
+
+                // If you want to display the response in the frontend, update the DOM here
+                const responseElement = document.getElementById("response");
+
+                // Display the extracted message in a readable format
+                responseElement.textContent = content;
+            })
+            .catch(error => {
+                // Handle errors
+                console.error("Error:", error);
+            });
+    });
+});
